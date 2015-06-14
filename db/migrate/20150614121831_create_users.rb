06db675615faf7e072419fc6e1e29af06a97a37e
p-guerin <guerin.pierre26@gmail.com>
2015-06-14 14:42:11 +0200

    Database: create users table

--- db/migrate/20150614121831_create_users.rb
+++ db/migrate/20150614121831_create_users.rb
@@ -1,6 +1,17 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :username, :null => false
+      t.string :email, :null => false
+      t.string :password_digest, :null => false
+      t.string :age
+      t.string :city
+      t.string :country
+      t.string :image
+      t.string :steam
+      t.string :facebook
+      t.string :twitter
+      t.string :youtube
 
       t.timestamps null: false
     end
